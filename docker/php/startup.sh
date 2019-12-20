9b30a994091ccce0cbc5adecc73ba9bde0ca1d61
cjc <cjc@cjc.id.au>
2019-12-20 11:44:01 +0000

    Fix startup.sh install script, move apt call to Dockerfile

--- docker/php/startup.sh
+++ docker/php/startup.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 echo 'Installing';
-apt install -y sudo ssh
-
-php install/step_01.php /app
+php /app/install/step_01.php /app
 
 chown -R www-data:www-data /app
 cp /db.inc /app/data/private/conf/
 
-sudo -u www-data php install/step_02.php /app/
-sudo -u www-data php install/step_03.php /app/
+sudo -u www-data php /app/install/step_02.php /app/
+sudo -u www-data php /app/install/step_03.php /app/
 
-php-fpm --nodaemonize
+#php-fpm --nodaemonize
