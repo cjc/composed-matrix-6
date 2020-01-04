6489ac6cec4de4a222b3b2e95061a00312e9e6a6
cjc <cjc@cjc.id.au>
2020-01-04 03:43:09 +0000

    Add support for a MATRIX_URL env var which is added to SQ_CONF_SYSTEM_ROOT_URLS on install. Add slight delay before install to give psql image a chance to come up

--- php/install.sh
+++ php/install.sh
@@ -5,6 +5,8 @@ if [ -f "$FILE" ]; then
   echo 'Matrix already installed, skipping'
 else 
   echo "$FILE does not exist"
+  echo "Waiting 5 seconds to give postgres a chance to come up because docker-compose healthcheck appears to not actually work"
+  sleep 5
   echo 'Copying Matrix source to volume'
   cp -R /src/* /app/
 
@@ -14,8 +16,13 @@ else
   chown -R www-data:www-data /app
   cp /db.inc /app/data/private/conf/
 
+  sed -i "s/^.*SQ_CONF_SYSTEM_ROOT_URLS',[ ]*'');/define('SQ_CONF_SYSTEM_ROOT_URLS', '"$MATRIX_URL"');/" /app/data/private/conf/main.inc
+
   sudo -u www-data php /app/install/step_02.php /app/
   sudo -u www-data php /app/install/step_03.php /app/
+
+  grep "SQ_CONF_SYSTEM_ROOT_URLS" /app/data/private/conf/main.inc
+
 fi
 
 php-fpm --nodaemonize
