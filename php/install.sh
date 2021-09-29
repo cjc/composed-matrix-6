32453b83460026b57586d609b7a4a515cda970a3
cjc <cjc@cjc.id.au>
2021-09-29 19:31:07 +1000

    Add uuid PHP extension. Change Dockerfile to use unzipped source to allow for npm install step. Stop removing ldap package due to dependency in new application_monitoring package.

--- php/install.sh
+++ php/install.sh
@@ -8,19 +8,16 @@ else
   echo "Waiting 5 seconds to give postgres a chance to come up because docker-compose healthcheck appears to not actually work"
   sleep 5
   echo 'Copying Matrix source to volume'
-  tar -xzf /src/matrix*.tgz -C /app/
+  #tar -xzf /src/matrix*.tgz -C /app/
+  cp -R /src/source/* /app
 
   #Remove exotic and extraneous packages that most people don't use. Comment out any lines you want to keep
-  rm -rf /app/packages/bulkmail
   rm -rf /app/packages/ecommerce
-  rm -rf /app/packages/ldap
+  #rm -rf /app/packages/ldap
   rm -rf /app/packages/marketo
   rm -rf /app/packages/sharepoint
   #rm -rf /app/packages/sugar
   rm -rf /app/packages/trim
-
-  #Hacky fix - remove two references in core to the bulkmail package to prevent errors caused by not installing bulkmail
-  sed -i '/bmail/d' /app/core/include/backend_common.inc
 
   #Extract all supplied extensions prior to installation
   find /src/packages/*.tgz -exec tar -xzf {} -C /app/ \;
